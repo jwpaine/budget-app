3569cf62469517534acfb9a989cc858897ccdf6f
John <mdibound@gmail.com>
2023-07-16 16:53:36 -0400

    Stripe subscription

--- app/routes/account.tsx
+++ app/routes/account.tsx
@@ -2,6 +2,8 @@ import { Link } from "@remix-run/react";
 
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
+import StripeCheckout from "react-stripe-checkout";
+
 import {
   Form,
   useCatch,
@@ -20,18 +22,68 @@ import { Decimal } from "@prisma/client/runtime";
 import { getAccount } from "~/models/account.server";
 import { getUserById } from "~/models/user.server";
 
+
+
 export async function loader({ request, params }: LoaderArgs) {
   const userId = await requireUserId(request);
-
+  const stripeKey = process.env.STRIPE_KEY as string
+  // if not stripe key, panic:
+  if (!stripeKey) {
+    throw new Error("STRIPE_KEY must be set");
+  } else {
+    console.log('stripe key: ', stripeKey)
+  }
   // const account = await getAccount({ userId });
-  const user = await getUserById( userId );
+  const user = await getUserById(userId);
 
-  return json({ userId, user });
+  return json({ userId, user, stripeKey });
 }
+
+// react useState hook accountState:
+
 
 export default function Budget() {
   const data = useLoaderData<typeof loader>();
 
+  const [accountState, setAccountState] = React.useState("details")
+
+  const subscription = useFetcher()
+
+
+
+
+  const handleStripeToken = async (response: any) => {
+
+    console.log("Received stripe response: ", response)
+
+    const r = subscription.submit(
+      {
+        //stripe customer
+        id: response.id, 
+        created: response.created,
+        email: response.email,
+        // card metadata
+        cardBrand: response.card.brand,
+        cardExpMonth: response.card.exp_month,
+        cardExpYear: response.card.exp_year,
+        cardId: response.card.id,
+        cardLast: response.card.last4,
+        cardName: response.card.name
+      },
+      { method: "post", action: "/subscription/create" }
+    );
+
+  }
+
+  const renderStripeCheckout = () => {
+    return <StripeCheckout
+      token={handleStripeToken}
+      stripeKey={data.stripeKey}
+      amount={0}
+      name="dollargrad.com"
+    // billingAddress="true"
+    />
+  }
 
 
   return (
@@ -40,6 +92,12 @@ export default function Budget() {
       <div className="p-2">
         <p className="text-white">Currently logged in as: {data?.user?.email}</p>
         {/* <p className="text-white">{JSON.stringify(data?.user)} </p> */}
+      </div>
+
+      <div className="p-2">
+       <span className="text-white">No Active Subscription. Subscribe to unlock more features.</span>
+        {renderStripeCheckout()}
+       
       </div>
       <Form action="/logout" method="post" className="p-2">
         <button
@@ -52,4 +110,9 @@ export default function Budget() {
 
     </main>
   );
+
+  if (accountState == "subscribe") return (
+    <></>
+  )
+
 }
---
+++ app/routes/subscription/create.tsx
@@ -0,0 +1,115 @@
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { requireUserId } from "~/auth.server";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+export async function action({ request, params }: ActionArgs) {
+
+    const createSubscription = async (customer: any, plan: any) => {
+        console.log('creating subscription')
+
+        let date = new Date(); 
+                  
+        let billing_cycle_anchor = new Date(date.getFullYear(), date.getMonth(), 0); 
+        billing_cycle_anchor.setMonth(billing_cycle_anchor.getMonth())
+        
+         let sub_data = {
+                customer: customer.id,
+                items: [
+                    {
+                        plan: plan.id
+                    }
+                ]
+        }
+
+        const subscription = await stripe.subscriptions.create(sub_data);
+
+        if (subscription.error) {
+            console.log(`subscription creation error: ${subscription.error}`)
+            return { subscription, error: subscription.error }
+        }
+        return { subscription, error: null}
+
+    }
+
+
+    const createPlan = async () => {
+        
+        console.log('creating product')
+ 
+
+        const product = await stripe.products.create({
+            name: 'Dollargrad Subscription',
+            type: 'service',
+        });
+
+        if(product.error) {
+            console.log("error creating product: ", product.error)
+            return { plan: null, error: product.error }
+        }
+
+        console.log('creating plan')
+
+        const plan = await stripe.plans.create({
+            currency: 'usd',
+            interval: 'month',
+            product: product.id,
+            nickname: `Subscription`,
+            amount_decimal: 700 // $7.00
+        });
+
+        if (plan.error) {
+            console.log("error creating plan: ", plan.error)
+            return { plan, error: plan.error }
+        }
+
+        return plan
+
+    }
+
+
+
+
+    const userId = await requireUserId(request);
+  
+    const formData = await request.formData();
+
+    const id = formData.get("id") as string
+    const created = formData.get("created") as string
+    const email = formData.get("email") as string
+    const cardBrand = formData.get("cardBrand") as string
+    const cardExpMonth = formData.get("cardExpMonth") as string
+    const cardExpYear = formData.get("cardExpYear") as string
+    const cardId = formData.get("cardId") as string
+    const cardLast = formData.get("cardLast") as string
+    const cardName = formData.get("cardName") as string
+
+    // create stripe customer
+    const customer = await stripe.customers.create({
+        email,
+        source: id
+    });
+
+    // if error, return json containing error:
+    if (customer.error) {
+        return json({ error: customer.error })
+    }
+
+    // create plan:
+    const plan = await createPlan()
+    if (plan.error) {
+        return json({ error: plan.error })
+    }
+    const subscription = await createSubscription(customer, plan)
+    if (subscription.error) {
+        return json({ error: subscription.error })
+    }
+
+    console.log("Subscription Created!")
+
+    // save stripe customer data to subscription table
+
+    
+    return redirect(`/account`)
+}
