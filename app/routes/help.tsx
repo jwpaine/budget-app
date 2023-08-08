3647a6f9e93f20ee57f00ab2ecbb99feda63ab04
John <mdibound@gmail.com>
2023-08-07 21:05:32 -0400

    Help center

--- app/routes/help.tsx
+++ app/routes/help.tsx
@@ -37,6 +37,7 @@ export async function action({ request, params }: ActionArgs) {
 
   const formData = await request.formData();
   const email = formData.get("email") as string;
+  const subject = formData.get("subject") as string;
   const message = formData.get("message") as string;
 
   console.log("Received:", email, message);
@@ -50,13 +51,13 @@ export async function action({ request, params }: ActionArgs) {
 
   // Set up AWS configuration
   AWS.config.update({
-  
+
     region: 'us-east-1'
   });
 
   var send_params = {
     Destination: { /* required */
-      
+
       ToAddresses: [
         'jpaine@dollargrad.com',
         /* more items */
@@ -64,37 +65,37 @@ export async function action({ request, params }: ActionArgs) {
     },
     Message: { /* required */
       Body: { /* required */
-       
+
         Text: {
-         Charset: "UTF-8",
-         Data: `Customer email: ${email}. Message: ${message}`
+          Charset: "UTF-8",
+          Data: `Customer email: ${email}. Message: ${message}`
         }
-       },
-       Subject: {
-        Charset: 'UTF-8',
-        Data: 'NEW SUPPORT REQUEST'
-       }
       },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: subject
+      }
+    },
     Source: 'noreply@dollargrad.com', /* required */
     ReplyToAddresses: [
-       email,
+      email,
       /* more items */
     ],
   };
-  
+
   // Create the promise and SES service object
-  var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(send_params).promise();
-  
+  var sendPromise = new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(send_params).promise();
+
   // Handle promise's fulfilled/rejected states
   return sendPromise.then(
-    function(data) {
+    function (data) {
       console.log(data.MessageId);
       return json({ success: true });
     }).catch(
-      function(err) {
-      console.error(err, err.stack);
-      return json({ error: err.message }, { status: 500 });
-    });
+      function (err) {
+        console.error(err, err.stack);
+        return json({ error: err.message }, { status: 500 });
+      });
 
 
   // Create a new SES object
@@ -143,7 +144,7 @@ export async function loader({ request, params }: LoaderArgs) {
 
 }
 
-const chatBubble = ({ message, type}: { message: string, type?: string }) => {
+const chatBubble = ({ message, type }: { message: string, type?: string }) => {
   return <span className={`flex rounded p-2 border border-slate-400 w-full my-2 text-xl ${type == 'customer' ? 'text-white bg-blue-500' : 'text-slate-800 bg-white'}`}>
     {message}
   </span>
@@ -156,6 +157,7 @@ export default function Help() {
   const [response, setResponse] = React.useState("")
 
   const [message, setMessage] = React.useState("")
+  const [tag, setTag] = React.useState("")
 
   const deliverMessage = useFetcher()
 
@@ -163,12 +165,14 @@ export default function Help() {
   const sendMessage = () => {
     // send email to support
 
-    console.log("sending message: ", response)
+    console.log("Message: ", response)
     console.log("Using email: ", data.account?.email || email)
+    console.log( `Subject: NEW SUPPORT REQUEST: ${tag}`)
 
     const r = deliverMessage.submit(
       {
         email: data.account?.email || email,
+        subject: `NEW SUPPORT REQUEST: ${tag}`,
         message: response
       },
       { method: "post", action: "/help" }
@@ -176,7 +180,29 @@ export default function Help() {
 
   }
 
+  const renderInput = () => {
+    if (!email && !data.account) return <div className="flex max-w-2xl w-full mt-1">
+      <input className="rounded border border-sky-500 w-full p-2" type="text" value={response} onChange={(e) => setResponse(e.target.value)} />
+      <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => {
+        if (!data.account && !email) {
+          setEmail(response)
+          setResponse("")
+          return
+        }
+        
+      }}>Send</button>
+    </div>
 
+    if (!message && tag) return <div className="flex max-w-2xl w-full mt-1">
+      <input className="rounded border border-sky-500 w-full p-2" type="text" value={response} onChange={(e) => setResponse(e.target.value)} />
+      <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => {
+        setMessage(response)
+        sendMessage()
+
+      }}>Send</button>
+    </div>
+
+  }
 
   return (
     <main className="flex flex-col justify-center items-center">
@@ -186,30 +212,33 @@ export default function Help() {
         {data.account ? chatBubble({ message: `It looks like you're logged in! We'll follow up with you at ${data.account.email}` }) : (
           chatBubble({ message: `What's a good email that we can use to follow up with you?` })
         )}
-        {data.account && chatBubble({ message: `What can we help you with?` })}
         {email && chatBubble({ message: email, type: "customer" })}
-        {email && chatBubble({ message: `We'll follow up with you at ${email}. Let us know how we can help!` })}
+        {email && chatBubble({ message: `We'll follow up with you at ${email}. Select an option below to get started:` })}
+        {data.account && chatBubble({ message: `What can we help you with? Select an option below to get started:` })}
+        {tag == "" && (data.account || email) && (
+          <>
+            <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => setTag("Billing")}>Billing</button>
+            <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => setTag("Login")}>Login</button>
+            <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => setTag("Budget")} >My Budget</button>
+            <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => setTag("Suggestion")} >Suggestions</button>
+            <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => setTag("Bug")} >Report a bug</button>
+            <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => setTag("Other")}>Other</button>
+
+          </>
+        )}
+        {tag && chatBubble({ message: tag, type: "customer" })}
+        {tag != "" && chatBubble({ message: `${tag}. Got it. Please provide a detailed message and our support team will reach back to you.` })}
         {message && chatBubble({ message: message, type: "customer" })}
         {message && deliverMessage?.data?.success && chatBubble({ message: `Message received! We'll follow up with you at ${email} ASAP!` })}
         {deliverMessage?.data?.error && chatBubble({ message: `We encountered an error when attempting to deliver your message. Our support team has been made aware of the issue. Please try again later.` })}
-       
+
 
 
       </section>
-      {!message && <div className="flex max-w-2xl w-full mt-1">
-          <input className="rounded border border-sky-500 w-full p-2" type="text" value={response} onChange={(e) => setResponse(e.target.value)} />
-          <button className="rounded text-white bg-blue-500 p-2 ml-1" type="button" onClick={() => {
-            if (!data.account && !email) {
-              setEmail(response)
-              setResponse("")
-              return
-            }
-            setMessage(response)
-            setResponse("")
-            sendMessage()
-          
-          }}>Send</button>
-        </div>}
+
+      {renderInput()}
+
+
     </main>
   );
 }
